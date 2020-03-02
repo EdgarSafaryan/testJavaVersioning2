768128dcb4d7e8bfe0cfd00e3d305b0d3cd8f47c
EdgarSafaryan <e.safaryan@it-students.fr>
2020-03-02 11:39:36 +0100

    troisieme changeemnt

--- src/fr/it_akademy/githubTest/Main.java
+++ src/fr/it_akademy/githubTest/Main.java
@@ -6,6 +6,7 @@ public class Main {
 	// write your code here
         System.out.println("hello world");
         System.out.println("hello world22222");
+        System.out.println("youppiiii");
 
     }
 }
