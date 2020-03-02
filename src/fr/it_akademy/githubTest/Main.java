e5627a6e1608a8c32962c97e1fd85379838d80f2
admin <admin@iMac3.local>
2020-03-02 11:32:17 +0100

    Initial commit

---
+++ src/fr/it_akademy/githubTest/Main.java
@@ -0,0 +1,10 @@
+package fr.it_akademy.githubTest;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("hello world");
+
+    }
+}
